2f00d2490c9a0160787e6ab1849b9364dedceea9
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-09-13 18:08:29 +0200

    add weapon that rotates towards mouse & changes z-index accordingly
    When the weapon is rotated "up" it is drawn behind the player. If it rotated down (between 90 and 270 degrees) it is drawn on top of the player.

---
+++ Choice Paralysis in Games/Assets/Scripts/GunLayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class GunLayerController : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _gunSpriteRenderer;
+
+        private void Start()
+        {
+            _gunSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            _gunSpriteRenderer.sortingOrder = 
+                transform.localEulerAngles.z > 90 && transform.localEulerAngles.z < 270 ? 2 : 1;
+
+            Debug.Log(transform.localEulerAngles.z);
+        }
+    }
+}
---
+++ Choice Paralysis in Games/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class LookAtMouse : MonoBehaviour
+    {
+        private UserInputController _userInputController;
+
+        private void Start()
+        {
+            _userInputController = UserInputController.Instance;
+        }
+
+        private void FixedUpdate()
+        {
+            transform.up = (Vector2) (_userInputController.MouseWorldPosition - transform.position).normalized;
+        }
+    }
+}
