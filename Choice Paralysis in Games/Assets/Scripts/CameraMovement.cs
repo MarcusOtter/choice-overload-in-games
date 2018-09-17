fc4a96faf4a8d28abd5d83c12fb1de5de864e0f4
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-09-17 12:28:45 +0200

    Add camera movement that follows mouse & player
    Still needs to be clamped to a max distance

---
+++ Choice Paralysis in Games/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraMovement : MonoBehaviour
+    {
+        [SerializeField] private float _smoothTime = 0.1f;
+        [SerializeField] private float _maxDistance = 4;
+
+        private UserInputController _userInput;
+        private Transform _playerTransform;
+
+        private Vector2 _velocity;
+
+        private void Start()
+        {
+            _userInput = UserInputController.Instance;
+            _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 midPoint = (_playerTransform.position + _userInput.MouseWorldPosition) / 2;
+
+            Vector2 damp = Vector2.SmoothDamp(transform.position, midPoint, ref _velocity, _smoothTime);
+            Vector3 targetPos = new Vector3(damp.x, damp.y, transform.position.z);
+            transform.position = targetPos;
+
+            /*
+            if (Vector2.Distance(_playerTransform.position, targetPos) < _maxDistance)
+            {
+                transform.position = targetPos;
+                Debug.Log("Fast");
+            }
+            else
+            {
+                damp = Vector2.SmoothDamp(transform.position, midPoint, ref _velocity, _smoothTime);
+                _velocity = Vector2.ClampMagnitude(_velocity, 0.01f);
+                targetPos = new Vector3(damp.x, damp.y, transform.position.z);
+                transform.position = targetPos;
+                Debug.Log("Slowed");
+            }
+            */
+        }
+    }
+}
