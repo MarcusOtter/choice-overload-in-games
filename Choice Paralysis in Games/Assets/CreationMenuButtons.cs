559fce00b45f0b6072c330ad386b705431339278
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-11-04 00:31:27 +0100

    Add menu UI system

--- Choice Paralysis in Games/Assets/CreationMenuButtons.cs
+++ Choice Paralysis in Games/Assets/CreationMenuButtons.cs
@@ -1,6 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class CreationMenuButtons : MonoBehaviour
 {
-    
+    [SerializeField] private List<Tab> _tabs;
+
+    private Tab _activeTab;
+
+    private void Start()
+    {
+        _activeTab = _tabs[0];
+        _activeTab.SetTabActive(true);
+    }
+
+    public void ActivateNewTab(int tabNumber)
+    {
+        _activeTab.SetTabActive(false);
+        _tabs[tabNumber].SetTabActive(true);
+        _activeTab = _tabs[tabNumber];
+    }
 }
--- Choice Paralysis in Games/Assets/Tab.cs
+++ Choice Paralysis in Games/Assets/Tab.cs
@@ -1,7 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Tab : MonoBehaviour
 {
     internal bool Visited;
+    internal bool Active;
 
+    [SerializeField] private Outline _selectedOutline;
+    [SerializeField] private GameObject _selectedIndicator;
+    [SerializeField] private GameObject _panel;
+
+    internal void SetTabActive(bool active)
+    {
+        _selectedOutline.enabled = active;
+        _selectedIndicator.SetActive(active);
+        _panel.SetActive(active);
+
+        if (active && !Visited)
+        {
+            Visited = true;
+        }
+    }
 }
