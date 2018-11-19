176051fbd0ec291e35c7ece19bac7eda19355cab
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-11-19 21:00:18 +0100

    Start mapping out data format

---
+++ Choice Paralysis in Games/Assets/FullCustomCharacter.cs
@@ -0,0 +1,22 @@
+﻿
+[System.Serializable]
+public class FullCustomCharacter
+{
+    // =============== General settings =============== //
+    internal readonly string CharacterName;
+    internal readonly int SkinTone; //  1: Brown  |  2: White  |  3: Red  |  4: Green
+    internal readonly string CatchPhrase;
+
+    // ==================== Clothes =================== //
+
+    // ===================== Hair ===================== //
+
+    // ==================== Weapon ==================== //
+
+    internal FullCustomCharacter(string characterName, int skinTone, string catchPhrase)
+    {
+        CharacterName = characterName;
+        SkinTone = skinTone;
+        CatchPhrase = catchPhrase;
+    }
+}
---
+++ Choice Paralysis in Games/Assets/Scripts/Data/Entry.cs
@@ -0,0 +1,6 @@
+﻿[System.Serializable]
+public class Entry
+{
+    private int _visitedTabs;
+    private float _customizationTime;
+}
