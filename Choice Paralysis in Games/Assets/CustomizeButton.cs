5532e03cb96dcae6d527fd4c80bb6659048a477c
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-11-21 22:29:05 +0100

    Add customizable skin & eye color

---
+++ Choice Paralysis in Games/Assets/CustomizeButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class CustomizeButton : MonoBehaviour
+{
+    [Header("Button setting")]
+    [SerializeField] private bool _changeColor;
+    [SerializeField] private bool _changeImage;
+
+    [Header("References")]
+    [SerializeField] private SpriteRenderer _imageToChange;
+
+    private Image _thisImage;
+
+    private void Start()
+    {
+        _thisImage = GetComponent<Image>();
+    }
+
+    // Called by pressing the button (preferrably on this object)
+    public void ApplyChange()
+    {
+        if (_changeColor)
+        {
+            _imageToChange.color = _thisImage.color;
+        }
+
+        if (_changeImage)
+        {
+            _imageToChange.sprite = _thisImage.sprite;
+        }
+    }
+}
