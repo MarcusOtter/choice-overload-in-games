961c756823abff4c3e0283d1cd2d35ed586b449b
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-12-10 22:08:27 +0100

    Add base for DataCollector.cs

---
+++ Choice Paralysis in Games/Assets/Scripts/Data/DataCollector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Data
+{
+    public class DataCollector : MonoBehaviour
+    {
+        internal static DataCollector Instance { get; private set; }
+
+        private void Awake()
+        {
+            SingletonCheck();
+        }
+
+        private void SingletonCheck()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
