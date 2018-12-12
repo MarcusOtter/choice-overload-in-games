2bd0b6a4281a5424a8baa66880b3dcc1285bc16e
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-12-12 01:35:46 +0100

    Make EnvironmentVariables secret & user defined

---
+++ Choice Paralysis in Games/Assets/Scripts/EnvironmentVariables_example.cs
@@ -0,0 +1,8 @@
+﻿namespace Scripts
+{
+    internal static class EnvironmentVariables_example
+    {
+        internal const string PlayerTag = "Player";
+        internal const string WebTaskUri = "https://your-webtask-url-here.com";
+    }
+}
