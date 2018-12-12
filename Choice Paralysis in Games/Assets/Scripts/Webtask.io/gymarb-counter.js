4da659346a5f0ab06ba16d7e9aa3414492793603
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-12-13 00:11:46 +0100

    Update outdated webtask code

--- Choice Paralysis in Games/Assets/Scripts/Webtask.io/gymarb-counter.js
+++ Choice Paralysis in Games/Assets/Scripts/Webtask.io/gymarb-counter.js
@@ -1,32 +1,42 @@
 // This file needs to be commented out for unity to compile.
-// Create this webtask at https://webtask.io/make, and modify the string 
-// EnvironmentVariables.WebTaskUri to be equal to the link provided at webtask.io.
+// Create this webtask at https://webtask.io/make, and modify the 
+// string EnvironmentVariables.WebTaskUri to be equal to the link provided at webtask.io.
 /*
-module.exports = function (context, callback)
+module.exports = function(context, callback)
 {
-    // Get data
-    context.storage.get(function (getError, data) 
+  // Get data
+  context.storage.get(function (getError, data)
+  {
+    // Ensure nothing is wrong with the data
+    if (getError) { return callback(getError); }
+    data = data || {"count": 0};
+    if (!data.count)
     {
-        // Ensure nothing is wrong with the data
-        if (getError) { return callback(getError); }
-        data = data || {"count": 0};
-        if (!data.count)
-        {
-            data.count = 0;
-        }
+      data.count = 0;
+    }
 
-        // Increment the counter
-        data.count++;
+    console.log(context.body_raw);
 
-        // Save the data
-        context.storage.set(data, {force: 1}, function (setError)
-        {
-            if (setError) { return callback(setError); }
-        });
+    if (context.body_raw === "1")
+    {
+      // Increment the counter
+      data.count++;
+    }
+    else if (context.body_raw === "-1")
+    {
+      // Reset the counter
+      data.count = 1;
+    }
 
-        // Return the new value.
-        // Important note: not 0-based index. First player is number 1.
-        callback(null, data.count);
+    // Save the data
+    context.storage.set(data, {force: 1}, function (setError)
+    {
+      if (setError) { return callback(setError); }
     });
+
+    // Return the new value.
+    // Important note: not 0-based index. First player is number 1.
+    callback(null, data.count);
+  });
 };
 */
