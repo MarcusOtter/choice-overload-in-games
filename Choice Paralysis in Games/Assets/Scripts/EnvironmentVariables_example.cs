fb8304630f22873d50b68df4fb1683e729a7fabc
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-01-22 09:18:00 +0100

    Correct variable name in example

--- Choice Paralysis in Games/Assets/Scripts/EnvironmentVariables_example.cs
+++ Choice Paralysis in Games/Assets/Scripts/EnvironmentVariables_example.cs
@@ -4,7 +4,7 @@
     {
         internal const string PlayerTag = "Player";
         internal const string DefaultCharacterName = "John Doe the Great";
-        internal const string WebTaskUri = "https://your-counter-webtask-url-here.com";
+        internal const string CounterWebTaskUri = "https://your-counter-webtask-url-here.com";
         internal const string DataWebTaskUri = "https://your-data-webtask-url-here.com";
     }
 }
